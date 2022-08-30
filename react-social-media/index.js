1a08813e3f4cb239e3046f6a1d57655d8d4411e8
Radwan Ahmed <radwan.hussein@protonmail.com>
2022-08-30 15:07:57 -0400

    d

--- react-social-media/index.js
+++ react-social-media/index.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 3000
 
 const io = new Server(server, {
     cors: {
-        origin: `localhost/:${PORT}`,
+        origin: `http://rad-social.herokuapp.com/:${PORT}`,
     }
 }, console.log('connected'));
 
